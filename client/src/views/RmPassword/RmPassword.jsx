93ad6e4718d7984c7fa804387a2359a7aa3fc954
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-22 11:33:39 +0100

    edit the profile route

---
+++ client/src/views/RmPassword/RmPassword.jsx
@@ -0,0 +1,10 @@
+import './RmPassword.css';
+const RmPassword = () => {
+	return (
+		<div className='.rm-container'>
+			<h1>We are working on it</h1>
+		</div>
+	)
+}
+
+export default RmPassword;
--- client/src/views/profile/Profile.jsx
+++ client/src/views/profile/Profile.jsx
@@ -6,7 +6,7 @@ const Profile = () => {
 		<div className='container'>
 			<div className='text-wrapper'>
 				<h1 className='name'>Youssef Bouryal</h1>
-				<p className='discription'>Beginner software developer, worked on this project from this <a href="#">tutorial</a> I added some custom things, such as the database, the structure of the code and many more. you can find the code in my <a href="">github</a></p>
+				<p className='discreption'>Beginner software developer, worked on this project from this <a href="https://www.youtube.com/watch?v=q9V7kLXvP3E&list=PLpPqplz6dKxXfVVta2i-kZQR_61ZKZmgJ&index=3&ab_channel=PedroTech" target={'_blank'}>tutorial</a>. I added some custom things, I design my own database, I change the structure of the code and many more. you can find the code in my <a href="https://github.com/thenew-programer/pass-manager" target={'_blank'}>github</a></p>
 			</div>
 		</div>
 	)
