d9942f16cd8521a759d9d96edde8322e2494943f
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-22 11:37:22 +0100

    add some function conttoller to send data

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,7 +1,6 @@
-import express from 'express';
-// import { authentification, random } from '../utils';
 import { insertToDB } from '../db/users.js';
 import { encrypt, decrypt } from '../utils/index.js';
+import { db } from '../index.js';
 
 
 export const addPass = (req, res) => {
@@ -9,13 +8,29 @@ export const addPass = (req, res) => {
 
 	const password = encryptedObj.password;
 	const emailUser = req.body.email_user;
-	const website = 'https://' + req.body.site;
+	const website = req.body.site;
 	const iv = encryptedObj.iv;
 
 	const state = insertToDB(password, emailUser, website, iv);
 	if (state === false) res.send("An Error accured!");
 	else res.send("Success");
 };
+
+export const showPass = (req, res) => {
+	db.query("SELECT * FROM PasswordUserSite;", (err, result) => {
+		if (err) {
+			console.log(err);
+		} else {
+			res.send(result);
+		}
+	})
+};
+
+export const decryptPass = (req, res) => {
+	const password = decrypt(req.body);
+	console.log(password);
+	res.send(JSON.stringify(password));
+}
 
 export const root = (req, res) => {
 	res.send("Hello world");
