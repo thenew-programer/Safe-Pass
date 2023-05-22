6918d1f33eb1557e087a25216a2cb657bede8ada
thenew-programer <109173505+thenew-programer@users.noreply.github.com>
2023-05-22 14:48:18 +0100

    Update users.js

--- server/src/db/users.js
+++ server/src/db/users.js
@@ -4,7 +4,7 @@ import { db } from '../index.js';
 
 export const insertToDB = (passwd, user, site, iv) => {
 
-	db.query(`INSERT INTO ${process.env.DB_NAME}(Password, User, Site, Iv) VALUES (?, ?, ?, ?)`,
+	db.query(`INSERT INTO ${process.env.DB_TABLE}(Password, User, Site, Iv) VALUES (?, ?, ?, ?)`,
 		[passwd, user, site, iv],
 		(err) => {
 			if (err) return false;
@@ -13,7 +13,7 @@ export const insertToDB = (passwd, user, site, iv) => {
 };
 
 export const getAll = () => {
-	db.query(`SELECT * FROM ${process.env.DB_NAME};`, (err, result) => {
+	db.query(`SELECT * FROM ${process.env.DB_TABLE};`, (err, result) => {
 		if (err) {
 			console.log(err);
 		} else {
