8c4319f373636b114b815d66a36278aa721ac666
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-22 11:35:05 +0100

    packege json

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Sidebar from './components/sidebar/sidebar';
+import Footer from './components/footer/Footer'
+
+const App = () => {
+
+	return (
+		<div className="App">
+			<Sidebar />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
