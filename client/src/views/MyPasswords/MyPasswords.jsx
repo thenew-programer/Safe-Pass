940217a2299fec8bc5b871cdce539ed7eb8caab6
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-22 10:26:19 +0100

    Add views or pages I have 3 pages in my project home where is a brief intro to the project, Add Password where you can add a password, My Password where You can see your passowrd

---
+++ client/src/views/MyPasswords/MyPasswords.jsx
@@ -0,0 +1,67 @@
+import './MyPasswords.css';
+import Axios from 'axios';
+import { useEffect, useState } from 'react';
+
+const SERVER = "http://localhost:3001/";
+
+const MyPasswords = () => {
+
+	const [passwordList, setPasswordList] = useState([]);
+	const [password, setPass] = useState('unknown');
+	useEffect(() => {
+		Axios.get(SERVER + 'showpasswords').then((response) => {
+			setPasswordList(response.data);
+		})
+	}, []);
+
+	const decryptPassword = (encryption) => {
+		Axios.post(SERVER + 'decrypt', {
+			password: encryption.password,
+			iv: encryption.iv
+		}).then((response) => {
+			setPasswordList(
+				passwordList.map((item) => {
+					return item.id == encryption.id ? {
+						id: item.id,
+						Password: item.Password,
+						Site: item.Site,
+						Iv: item.Iv,
+						pass: response.data
+					}
+						: item;
+				})
+			);
+		})
+	};
+
+	return (
+		<div className='container'>
+			<div className="passwords">
+				<div className="password">
+					{
+						passwordList.map((item, key) => {
+							return (
+								<div
+									className='pass'
+									onClick={() => {
+										decryptPassword({
+											password: item.Password,
+											iv: item.Iv,
+											id: item.id,
+										})
+									}}
+									key={key}>
+									<span>{item.Site}</span>
+									<br />
+									<span>pass: {item.pass}</span>
+								</div>
+							)
+						})
+					}
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default MyPasswords;
---
+++ client/src/views/profile/Profile.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './profile.css';
+
+const Profile = () => {
+	return (
+		<div className='container'>
+			<div className='text-wrapper'>
+				<h1 className='name'>Youssef Bouryal</h1>
+				<p className='discription'>Beginner software developer, worked on this project from this <a href="#">tutorial</a> I added some custom things, such as the database, the structure of the code and many more. you can find the code in my <a href="">github</a></p>
+			</div>
+		</div>
+	)
+}
+
+export default Profile;
