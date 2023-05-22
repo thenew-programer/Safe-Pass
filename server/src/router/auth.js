5c2bc559de09f821de78d47306829da879d74a46
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-22 11:36:34 +0100

    add router for showpass and decrypt pass

--- server/src/router/auth.js
+++ server/src/router/auth.js
@@ -1,8 +1,10 @@
 import  express  from 'express';
-import { addPass, root } from '../controllers/auth.js';
+import { addPass, root, showPass, decryptPass } from '../controllers/auth.js';
 
 
 export default (router) => {
 	router.post('/addPass', addPass);
+	router.post('/decrypt', decryptPass);
+	router.get('/showpasswords', showPass);
 	router.get('/', root)
 };
