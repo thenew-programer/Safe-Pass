c83b8185fadd35e98f277ff8b62898b4e0116756
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-24 02:45:52 +0100

    add the search feature, allow the user to search in the existing passwords

--- client/src/views/MyPasswords/MyPasswords.jsx
+++ client/src/views/MyPasswords/MyPasswords.jsx
@@ -7,11 +7,22 @@ const SERVER = "https://passwordmanager-l5wn.onrender.com/";
 const MyPasswords = () => {
 
 	const [passwordList, setPasswordList] = useState([]);
+	const [query, setQuery] = useState('');
+	const [backup, setBackup ] = useState([]);
+	const [stringSize, setStringSize] = useState(0);
 	useEffect(() => {
 		Axios.get(SERVER + 'showpasswords').then((response) => {
-			setPasswordList(response.data);
+			setPasswordList(response.data)
+			setBackup(response.data)
 		})
 	}, []);
+
+	useEffect(() => {
+		if (query.length === 0)
+		{
+		setPasswordList(backup);
+		}
+	}, [stringSize])
 
 	const decryptPassword = (encryption) => {
 		Axios.post(SERVER + 'decrypt', {
@@ -20,10 +31,12 @@ const MyPasswords = () => {
 		}).then((response) => {
 			setPasswordList(
 				passwordList.map((item) => {
+					item.pass = '⏺⏺⏺⏺⏺⏺';
 					return item.id == encryption.id ? {
 						id: item.id,
 						Password: item.Password,
 						Site: item.Site,
+						User: item.User,
 						Iv: item.Iv,
 						pass: response.data
 					}
@@ -33,32 +46,48 @@ const MyPasswords = () => {
 		})
 	};
 
+	const search = () => {
+		return passwordList.filter((item) => item.Site.toLowerCase().includes(query) ||
+											item.User.toLowerCase().includes(query));
+	}
+
 	return (
 		<div className='container'>
-			<h5>Click to show password</h5>
+			<h5>Search using website or email</h5>
+			<div className="search-field">
+				<input type="text" placeholder='search..'
+					onChange={event => {
+						setQuery(event.target.value);
+						setStringSize(query.length);
+						setPasswordList(search());
+					}} />
+			</div>
 			<div className="passwords">
-				<div className="password">
-					{
-						passwordList.map((item, key) => {
-							return (
-								<div
-									className='pass'
-									onClick={() => {
-										decryptPassword({
-											password: item.Password,
-											iv: item.Iv,
-											id: item.id,
-										})
-									}}
-									key={key}>
-									<span>{item.Site}</span>
-									<br />
-									<span>pass: {item.pass}</span>
-								</div>
-							)
-						})
-					}
-				</div>
+				{
+					passwordList.map((item, key) => {
+						if (!item.Site) item.Site = 'No Site';
+						return (
+							<div
+								className='pass'
+								onClick={() => {
+									decryptPassword({
+										password: item.Password,
+										iv: item.Iv,
+										id: item.id,
+									})
+								}}
+								key={key}>
+								<span className='site-span'>{item.Site}</span>
+								<br />
+								<span className='label'>Email-user</span>
+								<span className='email-span'>{item.User}</span>
+								<br />
+								<span className='label'>pass</span>
+								<span className='password-span'>{item.pass}</span>
+							</div>
+						)
+					})
+				}
 			</div>
 		</div>
 	);
