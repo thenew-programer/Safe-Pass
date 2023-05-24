259658963469ad2db02f0902b6bf591baf2ebb84
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-24 02:43:40 +0100

    Add the search function related to the controller addon

--- server/src/db/users.js
+++ server/src/db/users.js
@@ -4,7 +4,8 @@ import '../config.js';
 
 export const insertToDB = (passwd, user, site, iv) => {
 
-	db.query(`INSERT INTO ${process.env.DATABASE_TABLE}(Password, User, Site, Iv) VALUES (?, ?, ?, ?)`,
+	const query = `INSERT INTO ${process.env.DATABASE_TABLE}(Password, User, Site, Iv) VALUES (?, ?, ?, ?)`;
+	db.query(query,
 		[passwd, user, site, iv],
 		(err) => {
 			if (err) return false;
@@ -13,14 +14,29 @@ export const insertToDB = (passwd, user, site, iv) => {
 };
 
 export const getAll = () => {
-	db.query(`SELECT * FROM ${process.env.DATABASE_TABLE};`, (err, result) => {
+	const query = `SELECT * FROM ${process.env.DATABASE_TABLE};`;
+	db.query(query, (err, result) => {
 		if (err) {
 			console.log(err);
 		} else {
-			let data = result;
-			console.log(data);
-			return data;
+			return result;
 		}
 	});
 };
 
+export const isExist = (data) => {
+	const query = `SELECT * FROM ${process.env.DATABASE_TABLE} WHERE USER = ? AND Site = ?`;
+	db.query(query, [data.emailUser, data.website], (err, result) => {
+		if (err) console.error(err);
+		else {
+			if (result.length > 0) {
+				console.log('User exist');
+				return true;
+			}
+			else {
+				console.log('User doesn\'t exist');
+				return false;
+			}
+		}
+	})
+}
