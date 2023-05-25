328fd33d120c25a401e1254c20a8ff99f42d005f
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-26 00:37:47 +0100

    add method that returns the length of the database to use it in the front end 'dashboard'

--- server/src/controllers/auth.js
+++ server/src/controllers/auth.js
@@ -1,63 +1,100 @@
+import express from 'express';
 import { getAll, insertToDB, isExist } from '../db/users.js';
 import { encrypt, decrypt } from '../utils/index.js';
 import { db } from '../index.js';
 import '../config.js';
 
 
-export const addPass = async (req, res) => {
+export const addPass = (req, res) => {
 	const encryptedObj = encrypt(req.body.passwd);
-
 	const password = encryptedObj.password;
 	const emailUser = req.body.email_user;
 	const website = req.body.site;
 	website.toUpperCase();
 	const iv = encryptedObj.iv;
 
-	try {
+	// try {
+	// 	let isElementExist = isExist({
+	// 		emailUser: emailUser,
+	// 		website: website,
+	// 	});
+	// 	console.log('isElementExist = ' + isElementExist)
+	// 	// if the user doen't exist
+	// 	if (isElementExist === false) {
+	// 		const state = insertToDB(password, emailUser, website, iv);
+	//
+	// 		if (state === false) console.log("An Error accured! in db");
+	// 		else {
+	// 			res.send(JSON.stringify('Success'));
+	// 			console.log('Success | User does\'nt exist');
+	// 		}
+	// 	} else { 	// if the user exist
+	// 		res.send(JSON.stringify("Email already taken."));
+	// 		console.log("failure | User exist");
+	// 	}
+	// } catch (err) {
+	// 	console.log(err);
+	// }
 
-		let isElementExist = await isExist({
-			emailUser: emailUser,
-			website: website,
-		});
-
-
-		console.log('isElementExist = ' + isElementExist)
-
-		// if the user doen't exist
-		if (isElementExist === false) {
-			const state = insertToDB(password, emailUser, website, iv);
-
-			if (state === false) console.log("An Error accured! in db");
-			else {
-				res.send(JSON.stringify('Success'));
-				console.log('Success | User does\'nt exist');
+	isExist({ emailUser: emailUser, website: website })
+		.then((response) => {
+			if (response === false) {
+				insertToDB(password, emailUser, website, iv)
+					.then(() => {
+						res.send(JSON.stringify('Success'));
+						console.log('Success | User does\'nt exist');
+					}).catch((err) => console.error(err));
+			} else {
+				res.send(JSON.stringify("Email already taken."));
+				console.log("failure, user already exist!");
 			}
-		} else { 	// if the user exist
-			res.send(JSON.stringify("Email already taken."));
-			console.log("failure | User exist");
-		}
-	} catch (err) {
-		console.log(err);
-	}
+		}).catch((err) => {
+			console.error(err);
+			res.status(500).send('an error occured');
+		});
 };
+
+
 
 
 export const showPass = (req, res) => {
-	db.query(`SELECT * FROM ${process.env.DATABASE_TABLE};`, (err, result) => {
-		if (err) {
-			console.log(err);
-		} else {
-			res.send(result);
-		}
-	});
-	// const result = getAll();
-	// res.send(JSON.stringify(result));
+	// db.query(`SELECT * FROM ${process.env.DATABASE_TABLE};`, (err, result) => {
+	// 	if (err) {
+	// 		console.log(err);
+	// 	} else {
+	// 		res.send(result);
+	// 	}
+	// });
+	getAll()
+		.then((result) => {
+			res.send(JSON.stringify(result));
+		}).catch((err) => {
+			console.error(err)
+			res.status(500).send('An error occured in the db');
+		});
 };
+
+
 
 export const decryptPass = (req, res) => {
 	const password = decrypt(req.body);
 	res.send(JSON.stringify(password));
 }
+
+
+
+
+export const getPassCount = (req, res) => {
+	getAll().then((result) => {
+		console.log(result.length);
+		res.send(JSON.stringify(result.length));
+	}).catch((err) => {
+		console.error(err);
+		res.status(500).send('An error occured in the db');
+	})
+}
+
+
 
 export const root = (req, res) => {
 	res.send("Hello world");
