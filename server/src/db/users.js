a6cb11cbbcbb47ecab1c69ae3eec46dd913a0dcd
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-26 00:39:46 +0100

    add the method that talks with the database so the last method that get the length of the db can work

--- server/src/db/users.js
+++ server/src/db/users.js
@@ -3,43 +3,48 @@ import '../config.js';
 
 
 export const insertToDB = (passwd, user, site, iv) => {
+	return new Promise((resolve, reject) => {
 
-	const query = `INSERT INTO ${process.env.DATABASE_TABLE}(Password, User, Site, Iv) VALUES (?, ?, ?, ?)`;
-	db.query(query,
-		[passwd, user, site, iv],
-		(err) => {
-			if (err) return false;
-			else return true;
-		});
+		const query = `INSERT INTO ${process.env.DATABASE_TABLE}(Password, User, Site, Iv) VALUES (?, ?, ?, ?)`;
+		db.query(query, [passwd, user, site, iv],
+			(err) => {
+				if (err) {
+					reject();
+				} else {
+					resolve();
+				}
+			});
+	})
 };
+
+
 
 export const getAll = () => {
-	const query = `SELECT * FROM ${process.env.DATABASE_TABLE};`;
-	let returnValue = 0;
-	db.query(query, (err, result) => {
-		if (err) {
-			console.log(err);
-		} else {
-			returnValue = result;
-		}
-	});
-	return returnValue;
+	return new Promise((resolve, reject) => {
+		const query = `SELECT * FROM ${process.env.DATABASE_TABLE};`;
+		db.query(query, (err, result) => {
+			if (err) {
+				reject(err);
+			} else {
+				resolve(result);
+			}
+		});
+	})
 };
+
+
+
 
 export const isExist = (data) => {
 	return new Promise((resolve, reject) => {
 		const query = `SELECT * FROM ${process.env.DATABASE_TABLE} WHERE USER = ? AND Site = ?`;
 		db.query(query, [data.emailUser, data.website], (err, result) => {
 			if (err) {
-				console.error(err);
 				reject(err);
 			} else {
 				if (result.length > 0) {
-					console.log('User exist');
 					resolve(true);
-				}
-				else {
-					console.log('User doesn\'t exist');
+				} else {
 					resolve(false)
 				}
 			}
