e87301b579a3b1070355b641ae019572aae18e3c
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-26 05:41:51 +0100

    add some responsivity , switch from callback hell to promises and add some notification

--- client/src/views/addPassword/AddPassword.jsx
+++ client/src/views/addPassword/AddPassword.jsx
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import 'react-toastify/dist/ReactToastify.css';
 import Axios from "axios";
 import { useState } from 'react';
 import './addPassword.css';
 
-const server = 'https://passwordmanager-l5wn.onrender.com/addPass';
+const SERVER = 'https://passwordmanager-l5wn.onrender.com/addPass';
 
 
 
@@ -12,146 +14,166 @@ const AddPassword = () => {
 	let [password, setPassword] = useState('');
 	let [website, setWebsite] = useState('');
 	let [email_user, setEmail_user] = useState('');
-	let [addPassReturn, setAddPassReturn] = useState(-1);
+	const [isClicked, setIsClicked] = useState(false);
 
 
-	const clearThis = target => {
+
+	useEffect(() => {
+		window.addEventListener('keyup', (event) => {
+			if (event.key === 'Enter') {
+				addPasswordFunc();
+			}
+		});
+	}, [])
+
+
+
+	const clear = target => {
 		if (target.value != null)
 			target.value = "";
 	}
 
 
-	const fieldFailure = () => {
-		if (document.getElementById('error') !== null) return;
-		if (document.getElementById('errorF') !== null) {
-			document.getElementById('errorF').remove();
-		}
 
-		const email = document.getElementById('err');
-		email.insertAdjacentHTML('afterend', '<div id="error" class="failure-alert" >Fill all the fields</div>');
-	}
-
-	const failure = () => {
-		if (document.getElementById('errorF') !== null) return;
-		if (document.getElementById('error') !== null) {
-			document.getElementById('error').remove();
-		}
-
-		const email = document.getElementById('email');
-		email.insertAdjacentHTML('afterend', '<div id="errorF" class="failure-alert" >Email is already taken!</div>');
+	const notifyFieldFailure = () => {
+		toast.warn('All fields are required!', {
+			position: "top-center",
+			autoClose: 2000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "dark",
+		});
+		setTimeout(setIsClicked(false), 2000);
 	}
 
 
-	const success = () => {
-		if (document.getElementById('errorS') !== null) return;
-		if (document.getElementById('error') !== null) {
-			document.getElementById('error').remove();
-		}
 
-		const btn = document.getElementById('err');
-		btn.insertAdjacentHTML('afterend', '<div id="errorS" class="success-alert" >/ Password Added successfully.</div >');
+	const notifyFailure = () => {
+		toast.error('Password already exist!', {
+			position: "top-center",
+			autoClose: 2000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "dark",
+		});
 	}
+
+
+
+	const notifySuccess = () => {
+		toast.success("Password added successfully!", {
+			position: "top-center",
+			autoClose: 2000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: true,
+			draggable: true,
+			progress: undefined,
+			theme: "dark",
+		});
+	}
+
+
+
+	const isEmpty = () => {
+		return new Promise((resolve, reject) => {
+			if (website.length === 0 || email_user.length === 0 || password.length === 0) {
+				reject();
+			} else resolve();
+
+		})
+	}
+
+
+
+	const handleClick = () => {
+		if (!isClicked) {
+			addPasswordFunc();
+		}
+		setIsClicked(true);
+	}
+
+
+
+	const addPasswordFunc = () => {
+		isEmpty().then(() => {
+			addPass().then((response) => {
+				if (response === 1) {
+					setTimeout(clear(document.getElementById('site')), 800);
+					setTimeout(clear(document.getElementById('email')), 900);
+					setTimeout(clear(document.getElementById('pass')), 1000);
+					notifySuccess();
+				} else {
+					notifyFailure();
+				}
+			}).catch(() => console.error('failed'));
+		}).catch(() => notifyFieldFailure());
+	}
+
 
 
 	const addPass = () => {
-		Axios.post(server, {
-			email_user: email_user,
-			passwd: password,
-			site: website
-		}).then((response) => {
-			if (response.data == 'Success') {
-				console.log(response.data);
-				setAddPassReturn(1);
-			} else {
-				console.log(response.data);
-				setAddPassReturn(0);
-			}
-		});
-	};
-
-
-	const addPasswordFunc = (event) => {
-		if (event.key === 'Enter') {
-			isEmpty();
-			if (addPassReturn === -100) {
-				fieldFailure();
-			}
-			else {
-
-				addPass();
-				if (addPassReturn === 1) {
-					setTimeout(clearThis(document.getElementById('site')), 800);
-					setTimeout(clearThis(document.getElementById('email')), 900);
-					setTimeout(clearThis(document.getElementById('pass')), 1000);
-					success();
-				} else if (addPassReturn === 0) {
-					failure();
+		return new Promise((resolve, reject) => {
+			Axios.post(SERVER, {
+				email_user: email_user,
+				passwd: password,
+				site: website
+			}).then((response) => {
+				if (response.data == 'Success') {
+					console.log(response.data);
+					resolve(1)
+				} else {
+					console.log(response.data);
+					reject(0)
 				}
-			}
-		}
-	};
-
-	const isEmpty = () => {
-		if (website.length === 0 || email_user.length === 0 || password.length === 0) {
-			setAddPassReturn(-100);
-		}
+			}).catch(err => {
+				console.log(err);
+				reject();
+			})
+		})
 	}
 
 
-	useEffect(() => {
-		window.addEventListener('keyup', addPasswordFunc);
-		window.removeEventListener('keyup', addPasswordFunc);
-	}, [])
-
 
 	return (
+		<div className="pass-container">
+			<div className='passwd-form' >
 
-		<div className='passwd-form' >
+				<div className="none" id="err"></div>
+				<label htmlFor="site">Website</label>
+				<input type="text" id="site"
+					placeholder='e.g. linkedIn'
+					onChange={(event) => {
+						setWebsite(event.target.value);
+					}} required='required' />
 
-			<div className="none" id="err"></div>
-			<label htmlFor="site">Website</label>
-			<input type="text" id="site"
-				placeholder='e.g. linkedIn'
-				onChange={(event) => {
-					setWebsite(event.target.value);
-				}} required='required' />
+				<label htmlFor="email">Email/Username</label>
+				<input type="text" id="email"
+					placeholder="email/username"
+					onChange={(event) => {
+						setEmail_user(event.target.value);
+					}} required='required' />
 
-			<label htmlFor="email">Email/Username</label>
-			<input type="text" id="email"
-				placeholder="email/username"
-				onChange={(event) => {
-					setEmail_user(event.target.value);
-				}} required='required' />
+				<label htmlFor="pass">Password</label>
+				<input type="password" id="pass"
+					placeholder='e.g. pass123'
+					onChange={(event) => {
+						setPassword(event.target.value);
+					}} required />
+				<p>Passwords must contain at least eight characters, including at least 1 letter and 1 number. </p>
 
-			<label htmlFor="pass">Password</label>
-			<input type="password" id="pass"
-				placeholder='e.g. pass123'
-				onChange={(event) => {
-					setPassword(event.target.value);
-				}} required />
-			<p>Passwords must contain at least eight characters, including at least 1 letter and 1 number. </p>
-
-			<button type="submit" onClick={() => {
-				isEmpty();
-				if (addPassReturn === -100) {
-					fieldFailure();
-				}
-				else {
-
-					addPass();
-					if (addPassReturn === 1) {
-						setTimeout(clearThis(document.getElementById('site')), 800);
-						setTimeout(clearThis(document.getElementById('email')), 900);
-						setTimeout(clearThis(document.getElementById('pass')), 1000);
-						success();
-					} else if (addPassReturn == 0) {
-						failure();
-					}
-				}
-			}} id='btn'>
-				Save Credential
-			</button>
-		</div>
+				<button type="submit" onClick={handleClick} disabled={isClicked} id='btn'>
+					Save Password
+				</button>
+				<ToastContainer />
+			</div >
+		</div >
 	)
 }
 
