a111ffb5c49362a3dde2402907bceaa352647b80
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-26 04:05:27 +0100

    fix db connection issues

--- server/src/index.js
+++ server/src/index.js
@@ -12,12 +12,6 @@ let PORT = 0;
 // Connect to db
 export const db = mysql.createPool(process.env.DATABASE_URL);
 
-db.connect((err) => {
-	if (err) {
-		console.error('Error connection to MYSQL db:', err);
-	}
-	else console.log("Connected to sql successfully");
-})
 
 app.use(cors());
 app.use(bodyParser.json());
