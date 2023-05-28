ed2af4e840ce8b8b44d47e026d04b5eb4050ca16
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-28 20:28:40 +0100

    refactor css: using variable to make it easy for the themes feature

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
-import './App.css';
 import Sidebar from './components/sidebar/sidebar';
 import Footer from './components/footer/Footer'
+import DarkMode from './DarkMode/DarkMode';
+import './App.css';
 
 const App = () => {
 
 	return (
 		<div className="App">
 			<Sidebar />
+			<DarkMode />
 			<Footer />
 		</div>
 	);
