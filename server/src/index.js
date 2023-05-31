ae333472805af279919bbdffda93c831e11dd8bd
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-31 15:32:04 +0100

    debugging

--- server/src/index.js
+++ server/src/index.js
@@ -18,7 +18,6 @@ export const db = mysql.createPool(process.env.DATABASE_URL);
 mongoose.Promise = Promise;
 mongoose.connect(process.env.USERS_DATABASE_URL);
 mongoose.connection.on('connected', console.log('Connected Successfully'));
-mongoose.connection.on('error', (err) => console.log(err));
 
 
 app.use(cors());
