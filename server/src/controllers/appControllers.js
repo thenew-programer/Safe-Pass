a0719083fa5e4f6a3e6e244ab55705e567004389
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-06-01 00:13:56 +0100

    refactoring: using async await indtead then catch

--- server/src/controllers/appControllers.js
+++ server/src/controllers/appControllers.js
@@ -9,38 +9,10 @@ import { getError } from '../utils/users.js';
 
 
 export const addPass = async (req, res, next) => {
-	// const { password, iv } = encrypt(req.body.passwd);
-	// const emailUser = req.body.email_user;
-	// const website = req.body.site;
-	//
-	//
-	// isExist({ emailUser: emailUser, website: website })
-	// 	.then((response) => {
-	// 		console.log('return after isExist');
-	// 		if (response === false) {
-	// 			insertToDB({
-	// 				passwd: password,
-	// 				site: website,
-	// 				user: emailUser,
-	// 				iv: iv
-	// 			}).then(() => {
-	// 				console.log('Success | User does\'nt exist');
-	// 				return res.status(200).send(JSON.stringify('Success'));
-	// 			}).catch((err) => console.error(err));
-	// 		} else {
-	// 			console.log("failure, user already exist!");
-	// 			return res.send(JSON.stringify("Email already taken."));
-	// 		}
-	// 	}).catch((err) => {
-	// 		console.error(err);
-	// 		return res.status(500).send('Failed to add password');
-	// 	});
-
 	try {
 		const { password, iv } = encrypt(req.body.passwd);
 		const emailUser = req.body.email_user;
 		let website = req.body.site;
-		website.toUpperCase();
 
 		const response = await isExist({ emailUser: emailUser, website: website });
 
@@ -62,14 +34,6 @@ export const addPass = async (req, res, next) => {
 
 
 export const showPass = async (req, res, next) => {
-	// getAll()
-	// 	.then((result) => {
-	// 		res.send(JSON.stringify(result));
-	// 	}).catch((err) => {
-	// 		console.error(err)
-	// 		res.status(500).send('Failed to get passwords');
-	// 	});
-
 	try {
 		const response = await getAll();
 
@@ -83,26 +47,20 @@ export const showPass = async (req, res, next) => {
 
 
 export const decryptPass = async (req, res) => {
-	const password = decrypt(req.body);
 
-	return res.send(JSON.stringify(password));
+	const password = decrypt(req.body.password);
 
+	return res.status(200).send(JSON.stringify(password));
 }
 
 
 
 export const getPassCount = async (req, res, next) => {
-	// getAll().then((result) => {
-	// 	res.send(JSON.stringify(result.length));
-	// }).catch((err) => {
-	// 	console.error(err);
-	// 	res.status(500).send('Failed to get passwords count');
-	// })
-
 	try {
 		const response = await getAll();
 
 		return res.status(200).send(JSON.stringify(response.length));
+
 	} catch (err) {
 		console.error(err);
 		next(getError('SERVER FAILED', 500));
@@ -139,45 +97,45 @@ export const removePass = async (req, res, next) => {
 
 
 export const updatePass = async (req, res, next) => {
-	console.log(req.body);
-	isExist({ emailUser: req.body.email, website: req.body.site })
-		.then((response) => {
-			if (response !== false) {
-				updatePassdb({ password: req.body.newPass, id: response[0].id })
-					.then(() => {
-						return res.send('Success')
-					})
-					.catch(err => {
-						console.error(err);
-						return res.status(500).send("Failed to update.")
-					})
-			} else {
-				console.log('Failed to update: pass doesnt exist')
-				return res.send("Passowrd doesn't exist!");
-			}
-		})
-		.catch(err => {
-			console.error(err);
-			return res.status(500).send("Failed to update.")
-		});
+	// console.log(req.body);
+	// isExist({ emailUser: req.body.email, website: req.body.site })
+	// 	.then((response) => {
+	// 		if (response !== false) {
+	// 			updatePassdb({ password: req.body.newPass, id: response[0].id })
+	// 				.then(() => {
+	// 					return res.send('Success')
+	// 				})
+	// 				.catch(err => {
+	// 					console.error(err);
+	// 					return res.status(500).send("Failed to update.")
+	// 				})
+	// 		} else {
+	// 			console.log('Failed to update: pass doesnt exist')
+	// 			return res.send("Passowrd doesn't exist!");
+	// 		}
+	// 	})
+	// 	.catch(err => {
+	// 		console.error(err);
+	// 		return res.status(500).send("Failed to update.")
+	// 	});
+	//
 
+	try {
 
-	// try {
-	//
-	// 	const response = await isExist({ emailUser: req.body.email, website: req.body.site })
-	//
-	// 	if (response === false) {
-	// 		console.log('Failed to update: pass doesnt exist')
-	// 		return res.status(400).send("Password doesn't exist!");
-	// 	}
-	//
-	// 	await updatePassdb({ password: req.body.newPass, id: response[0].id })
-	// 	res.status(200).send('Success');
-	//
-	// } catch (err) {
-	// 	console.error(err);
-	// 	next(getError('SERVER FAILED', 500));
-	// }
+		const response = await isExist({ emailUser: req.body.email, website: req.body.site })
+
+		if (response === false) {
+			console.log('Failed to update: pass doesnt exist')
+			return res.status(400).send("Password doesn't exist!");
+		}
+
+		await updatePassdb({ password: req.body.newPass, id: response[0].id })
+		res.status(200).send('Success');
+
+	} catch (err) {
+		console.error(err);
+		next(getError('SERVER FAILED', 500));
+	}
 }
 
 
