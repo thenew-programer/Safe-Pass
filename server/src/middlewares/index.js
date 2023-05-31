991032370f4bdfea8e73cca25d90c307709a37da
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-31 15:24:19 +0100

    middlewares : isAuthenticated : global middleware each time the user request info from the server it checked if authenticated or not; isOwner: check for account ownership; errHandler: global error handler; pathErrorHandler: 404 not found handler

---
+++ server/src/middlewares/index.js
@@ -0,0 +1,69 @@
+import {  getUserBySessionToken } from '../db/users.js'
+import lodash from 'lodash';
+import { getError } from '../utils/users.js';
+
+
+const { get, merge } = lodash;
+
+export const errHandler = (err, req, res, next) => {
+	res.status(err.statusCode).json({
+		statusCode: err.statusCode,
+		message: err.message
+	});
+}
+
+
+export const pathErrHandler = (req, res, next) => {
+	const err = getError('Not Found', 404);
+	next(err);
+}
+
+
+export const isAuthenticated = async (req, res, next) => {
+	try {
+		const sessionToken = req.cookies['SAFE-PASS'];
+
+		if (!sessionToken) {
+			return res.sendStatus(403);
+		}
+
+		const user = await getUserBySessionToken(sessionToken);
+
+		if (!user) {
+			return res.sendStatus(403);
+		}
+
+		merge(req, { identity: user });
+		next();
+
+	} catch (err) {
+		console.error(err);
+		next(getError('SERVER FAILED', 500));
+	}
+
+}
+
+
+
+export const isOwner = async (req, res, next) => {
+	try {
+		const { id } = req.params;
+
+		const currentUserId = get(req, 'identity._id');
+
+		if (!currentUserId) {
+			return res.sendStatus(403);
+		}
+
+		if (currentUserId.toString() !== id) {
+			return res.sendStatus(403);
+		}
+
+		next();
+
+	} catch (err) {
+		console.error(err);
+		next(getError('SERVER FAILED', 500));
+	}
+}
+
