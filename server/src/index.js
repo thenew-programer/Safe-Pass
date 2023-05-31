ceb5cf9e76081688bc801f12fda2cd88e91fcfe5
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-31 15:26:07 +0100

    add new connection to mongodb as well as a new global middleware errHandler and a new global middleware isAuthenticated

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,11 @@ import express from 'express';
 import http from 'http';
 import bodyParser from 'body-parser';
 import mysql from 'mysql2';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
 import cors from 'cors';
+import { errHandler, isAuthenticated } from './middlewares/index.js';
 import router from './router/index.js';
 import './config.js'
 
@@ -11,10 +15,17 @@ let PORT = 0;
 
 // Connect to db
 export const db = mysql.createPool(process.env.DATABASE_URL);
+mongoose.Promise = Promise;
+mongoose.connect(process.env.USERS_DATABASE_URL);
+mongoose.connection.on('connected', console.log('Connected Successfully'));
+mongoose.connection.on('error', (err) => console.log(err));
 
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(cookieParser());
+app.use(compression());
+app.use(isAuthenticated);
 
 
 const server = http.createServer(app);
@@ -25,4 +36,4 @@ server.listen(PORT = (process.env.PORT || 3001), () => {
 
 
 app.use('/', router());
-
+app.use(errHandler);
