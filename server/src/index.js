af56b141e49f0fc80e7e6339b7efdfec8e57691e
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-05-31 19:49:23 +0100

    debugging

--- server/src/index.js
+++ server/src/index.js
@@ -21,6 +21,7 @@ mongoose.connect(process.env.USERS_DATABASE_URL);
 
 app.use(cors());
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 app.use(compression());
 
