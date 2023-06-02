b70efe66d6c956095e6ff9a181cb69382eb412d6
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-06-02 21:39:01 +0100

    cookie issues

--- server/src/controllers/authentification.js
+++ server/src/controllers/authentification.js
@@ -1,26 +1,27 @@
-import { createUser, deleteUserById, getUserByEmail, getUserById } from '../db/users.js';
-import { authentification, getError, random } from '../utils/users.js';
-import { createTable, removeTable } from '../db/app.js';
-
+import {
+	createUser, deleteUserById, getUserByEmail, getUserById,
+} from "../db/users.js";
+import { authentification, getError, random } from "../utils/users.js";
+import { createTable, removeTable } from "../db/app.js";
 
 export const register = async (req, res) => {
 	try {
-		console.log('register method called!');
+		console.log("register method called!");
 		console.log(req.body);
 		const email = req.body.email;
 		const password = req.body.password;
 		const username = req.body.username;
 
 		if (!email || !password || !username) {
-			return res.status(400).send('no data provided');
+			return res.status(400).send("no data provided");
 		}
 
 		const existingUser = await getUserByEmail(email);
 
 		if (existingUser) {
-			return res.status(400).send('user already exist');
+			return res.status(400).send("user already exist");
 		}
-		console.log('user is not there');
+		console.log("user is not there");
 
 		const salt = random();
 		const user = await createUser({
@@ -32,66 +33,60 @@ export const register = async (req, res) => {
 			},
 		});
 
-
 		const registeredUser = await getUserByEmail(email);
-		const userTable = 'user' + registeredUser._id.toString().slice(1, 7);
+		const userTable = "user" + registeredUser._id.toString().slice(1, 7);
 		registeredUser.userTable = userTable;
 		registeredUser.save();
 
-		console.log('user created successfully');
+		console.log("user created successfully");
 		await createTable(userTable);
-		console.log('table created successfully');
+		console.log("table created successfully");
 
 		return res.status(201).json(registeredUser).end();
 	} catch (err) {
 		console.error(err);
-		console.log('error while registring user');
+		console.log("error while registring user");
 		return res.status(500);
 	}
-}
-
-
+};
 
 export const login = async (req, res) => {
 	try {
 		const { email, password } = req.body;
 		if (!email || !password) {
-			return res.status(400).send('no credential provided');
+			return res.status(400).send("no credential provided");
 		}
 
-		const user = await getUserByEmail(email).select('+authentification.salt +authentification.password +userTable');
+		const user = await getUserByEmail(email).select(
+			"+authentification.salt +authentification.password +userTable"
+		);
 		if (!user) {
-			return res.status(400).send('no user registred under these credentials');
+			return res.status(400).send("no user registred under these credentials");
 		}
 
 		const expectedHash = authentification(user.authentification.salt, password);
 		if (expectedHash !== user.authentification.password) {
-			return res.status(400).send('incorrect password');
+			return res.status(400).send("incorrect password");
 		}
 
 		const salt = random();
-		user.authentification.sessionToken = authentification(salt, user._id.toString());
+		user.authentification.sessionToken = authentification(
+			salt,
+			user._id.toString()
+		);
 		await user.save();
 
 		const tomorrow = new Date();
 		tomorrow.setDate(tomorrow.getDate() + 1);
-		return res.status(200)
-			.cookie('__pass', user.authentification.sessionToken, {
-				sameSite: 'none',
-				secure: true,
-				httpOnly: true,
-				path: '/',
-				expires: tomorrow,
-				domain: '.onrender.com'
-			})
-			.send('welcome');
-
+		return res
+			.status(200)
+			.cookie("__pass", user.authentification.sessionToken)
+			.send("welcome");
 	} catch (err) {
 		console.error(err);
 		return res.status(500);
 	}
 };
-
 
 export const deleteUser = async (req, res) => {
 	try {
@@ -106,8 +101,7 @@ export const deleteUser = async (req, res) => {
 		console.error(err);
 		return res.status(500).json({ err: err });
 	}
-}
-
+};
 
 export const updateUser = async (req, res, next) => {
 	try {
@@ -115,14 +109,16 @@ export const updateUser = async (req, res, next) => {
 		const { password } = req.body;
 
 		if (!password) {
-			return res.status(400).send('unable to update password').end();
+			return res.status(400).send("unable to update password").end();
 		}
 
-		const user = await getUserById(id).select('+authentification.password +authentification.salt');
+		const user = await getUserById(id).select(
+			"+authentification.password +authentification.salt"
+		);
 
 		const expectedPass = authentification(user.authentification.salt, password);
 		if (expectedPass !== user.authentification.password) {
-			return res.status(400).send('old password incorrect').end();
+			return res.status(400).send("old password incorrect").end();
 		}
 
 		salt = random();
@@ -130,13 +126,12 @@ export const updateUser = async (req, res, next) => {
 		user.authentification.salt = salt;
 		user.save();
 
-		return res.status(200).send('passoword reset successfully').end();
-
+		return res.status(200).send("passoword reset successfully").end();
 	} catch (err) {
 		console.error(err);
-		next(getError('SERVER STATUS', 500));
+		next(getError("SERVER STATUS", 500));
 	}
-}
+};
 
 export const getUser = async (req, res, next) => {
 	try {
@@ -145,6 +140,6 @@ export const getUser = async (req, res, next) => {
 		return res.status(200).json(user).end();
 	} catch (err) {
 		console.log(err);
-		next(getError('Server STATUS', 500));
+		next(getError("Server STATUS", 500));
 	}
-}
+};
