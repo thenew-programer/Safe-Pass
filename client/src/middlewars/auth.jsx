483b37832e50ef1b8dcac1f5ae35b141f1d8ca35
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-06-02 23:36:10 +0100

    modify millware so it can use the cookies received from the user using the getCookie hook

--- client/src/middlewars/auth.jsx
+++ client/src/middlewars/auth.jsx
@@ -1,16 +1,21 @@
 import Axios from "axios";
+import { getCookies } from "../hooks/setCookies";
 
 const SERVER = 'https://passwordmanager-l5wn.onrender.com/auth';
 
 export const isAuthenticated = () => {
 	return new Promise((resolve, reject) => {
-		Axios.get(SERVER, { withCredentials: true })
-			.then((response) => {
-				if (response.status === 200) {
-					resolve();
-				} else {
-					reject();
-				}
-			}).catch(() => reject());
+		const cookies = '__pass=' + getCookies('__pass');
+		Axios.get(SERVER, {
+			headers: {
+				'Cookie': cookies,
+			}
+		}).then((response) => {
+			if (response.status === 200) {
+				resolve();
+			} else {
+				reject();
+			}
+		}).catch(() => reject());
 	})
 }
