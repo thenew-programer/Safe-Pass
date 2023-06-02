fe4ad1fad825bcf743977433255644ef826bd35d
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-06-03 00:20:22 +0100

    auth middleware

--- client/src/middlewars/auth.jsx
+++ client/src/middlewars/auth.jsx
@@ -1,14 +1,13 @@
 import Axios from "axios";
-import { getCookies } from "../hooks/setCookies";
+import GetCookies  from "../hooks/setCookies";
 
 const SERVER = 'https://passwordmanager-l5wn.onrender.com/auth';
 
 export const isAuthenticated = () => {
 	return new Promise((resolve, reject) => {
-		const cookies = '__pass=' + getCookies('__pass');
 		Axios.get(SERVER, {
-			headers: {
-				'Cookie': cookies,
+			params: {
+				'cookies': GetCookies('__pass'),
 			}
 		}).then((response) => {
 			if (response.status === 200) {
