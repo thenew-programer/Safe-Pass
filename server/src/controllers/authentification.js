c73d9adef9c3b68d1e39ede027b84336ce320fa1
Youssef Bouryal <youssefbouryal02@gmail.com>
2023-06-05 01:00:34 +0100

    cookies issues

--- server/src/controllers/authentification.js
+++ server/src/controllers/authentification.js
@@ -76,6 +76,7 @@ export const login = async (req, res) => {
 			.cookie('__pass', JSON.stringify(user.authentification.sessionToken), {
 				expires: tomorrow,
 				sameSite: 'none',
+				secure: true,
 			})
 			.send('welcome')
 	} catch (err) {
